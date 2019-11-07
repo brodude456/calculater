0c972e90037cfa4ec81a1338d0d257a9061ceb8d
brodude456 <fatihefeememisevic@gmail.com>
2019-11-07 15:36:50 +0100

    hmmm

--- calculater/calculater.py
+++ calculater/calculater.py
@@ -5,16 +5,21 @@ screen.title("CALCULATOR")
 screen.config(background='Dark gray')
 textin=StringVar()
 value_to_calc= ""
+operetor_list1=operetor_list1 
 
 def clickbut(character):
      global value_to_calc
      try:
-        if not (value_to_calc[-2] in ["/","*","+","-"] and value_to_calc[-1] in ["/","*","+","-"] and character in ["/","*","+","-"]) and not (value_to_calc[-1] in ["/","*","+","-","."] and character==".") and not (value_to_calc[-1] in ["/","*","+","-"] and character in ["/","*"]) and not(value_to_calc[-1]=="." and character in ["/","*","+","-"]):
+        if not (value_to_calc[-2] in operetor_list1 and value_to_calc[-1] in operetor_list1 and character in operetor_list1) and 
+        not (value_to_calc[-1] in ["/","*","+","-","."] and character==".") and not (value_to_calc[-1] in operetor_list1 and character in ["/","*"]) 
+        and not(value_to_calc[-1]=="." and character in operetor_list1):
             value_to_calc= value_to_calc + str(character)
             textin.set(value_to_calc)
      except:
         try:
-            if not (value_to_calc[-1] in ["/","*","+","-"] and character in ["/","*","+","-"]) and not (value_to_calc[-1] in ["/","*","+","-","."] and character==".") and not (value_to_calc[-1] in ["/","*","+","-"] and character in ["/","*"] and not(value_to_calc[-1]=="." and character in ["/","*","+","-"])):
+            if not (value_to_calc[-1] in operetor_list1 and character in operetor_list1) and not 
+            (value_to_calc[-1] in ["/","*","+","-","."] and character==".") and not (value_to_calc[-1] in operetor_list1 
+            and character in ["/","*"] and not(value_to_calc[-1]=="." and character in operetor_list1)):
                 value_to_calc= value_to_calc + str(character)
                 textin.set(value_to_calc)
         except:
@@ -41,23 +46,33 @@ def Result():
             operetor_list=[]
             num=""
             for i in value_to_calc:
-                if num!="" and i in ["/","*","+","-"]:
+                if num!="" and i in operetor_list1:
                     operetor_list.append(i)
                     num_list.append(num)
                     num=""
                 else:num+=i
             num_list.append(num)
             print(num_list,operetor_list)
-            value_to_calc=float(num_list[0])
+            
+            for ind,oper in enumerate(operetor_list):
+                if oper=="*":
+                    num_list[ind:ind+2]=str(int(num_list[ind])*int(num_list[ind+1]))
+                    num_list[ind:ind+2] = [''.join(num_list[ind:ind+2])]
+                    operetor_list.remove(oper)
+                elif oper=="/":
+                    num_list[ind:ind + 2] = str(int(num_list[ind]) / int(num_list[ind + 1]))
+                    num_list[ind:ind+2] = [''.join(num_list[ind:ind+2])]
+                    operetor_list.remove(oper)
+            print(operetor_list,num_list)
+            
+            value_to_calc = int(num_list[0])
+
             for ind,operater in enumerate(operetor_list):
                 if operater=="+":
-                    value_to_calc+=float(num_list[ind+1])
-                if operater=="-":
-                    value_to_calc-=float(num_list[ind+1])
-                if operater=="/":
-                    value_to_calc/=float(num_list[ind+1])
-                if operater=="*":
-                    value_to_calc*=float(num_list[ind+1])
+                    value_to_calc+=int(num_list[ind+1])
+                elif operater=="-":
+                    value_to_calc-=int(num_list[ind+1])
+            
             value_to_calc=str(value_to_calc)
             textin.set(value_to_calc)
     except:
@@ -65,36 +80,16 @@ def Result():
 
 metext=Entry(screen, font=("Courier New", 30, 'bold'), textvar=textin, width=25, bd=5, bg='powder blue')
 metext.pack()
-but1=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(1), text="1", font=("Courier New", 16, 'bold'))
-but1.place(x=10,y=100)
-but2=Button(screen, padx=14, pady=14, bd=4, bg='white', command=lambda:clickbut(2), text="2", font=("Courier New", 16, 'bold'))
-but2.place(x=10,y=170)
-but3=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(3), text="3", font=("Courier New", 16, 'bold'))
-but3.place(x=10,y=240)
-but4=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(4), text="4", font=("Courier New", 16, 'bold'))
-but4.place(x=75,y=100)
-but5=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(5), text="5", font=("Courier New", 16, 'bold'))
-but5.place(x=75,y=170)
-but6=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(6), text="6", font=("Courier New", 16, 'bold'))
-but6.place(x=75,y=240)
-but7=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(7), text="7", font=("Courier New", 16, 'bold'))
-but7.place(x=140,y=100)
-but8=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(8), text="8", font=("Courier New", 16, 'bold'))
-but8.place(x=140,y=170)
-but9=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(9), text="9", font=("Courier New", 16, 'bold'))
-but9.place(x=140,y=240)
-but0=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(0), text="0", font=("Courier New", 16, 'bold'))
-but0.place(x=10,y=310)
+design_dict={"0":(10,310),"1":(10,100),"2":(10,170),"3":(10,240),"4":(75,100),"5":(75,170),"6":(75,240)
+            ,"7":(140,100),"8":(140,170),"9":(140,240),"+":(205,100),"-":(205,170),"/":(205,310),"*":(205,240)}
+
+for num in design_dict:
+    but=Button(screen, padx=14, pady=14, bd=4, bg='white',command=lambda:clickbut(num),text=num, font=("Courier New", 16, 'bold'))
+    but.place(x=design_dict[num][0],y=design_dict[num][1])
+    print(design_dict[num])
+
 butdot=Button(screen, padx=47, pady=14, bd=4, bg='white',command=lambda:clickbut("."), text=".", font=("Courier New", 16, 'bold'))
 butdot.place(x=75,y=310)
-butpl=Button(screen, padx=14, pady=14, bd=4, bg='white', text="+",command=lambda:clickbut("+"), font=("Courier New", 16, 'bold'))
-butpl.place(x=205,y=100)
-butsub=Button(screen, padx=14, pady=14, bd=4, bg='white', text="-",command=lambda:clickbut("-"), font=("Courier New", 16, 'bold'))
-butsub.place(x=205,y=170)
-butml=Button(screen, padx=14, pady=14, bd=4, bg='white', text="*",command=lambda:clickbut("*"), font=("Courier New", 16, 'bold'))
-butml.place(x=205,y=240)
-butdiv=Button(screen, padx=14, pady=14, bd=4, bg='white', text="/",command=lambda:clickbut("/"), font=("Courier New", 16, 'bold'))
-butdiv.place(x=205,y=310)
 butclear=Button(screen, padx=14, pady=49, bd=4, bg='white', text="CE",command=lambda:Clear(), font=("Courier New", 16, 'bold'))
 butclear.place(x=270,y=100)
 butDel=Button(screen, padx=14, pady=49, bd=4, bg='white', text="DEL",command=lambda:Del(), font=("Courier New", 16, 'bold'))
